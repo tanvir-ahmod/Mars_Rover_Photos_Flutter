b51a1a8e6cc9b49d44b8c0418203daeea8ccab9e
Tanvir Ahmod <tanvirahmodcse@gmail.com>
2020-12-22 21:30:28 +0600

    Implement showing image to gallery

--- lib/show_image/show_rover_images.dart
+++ lib/show_image/show_rover_images.dart
@@ -5,6 +5,7 @@ import 'package:mars_rover_image_flutter/bloc/show_image/show_image_bloc.dart';
 import 'package:mars_rover_image_flutter/bloc/show_image/show_image_event.dart';
 import 'package:mars_rover_image_flutter/bloc/show_image/show_image_state.dart';
 import 'package:mars_rover_image_flutter/models/query_model.dart';
+import 'package:mars_rover_image_flutter/models/rover_data.dart';
 import 'package:mars_rover_image_flutter/show_image/custom_app_bar.dart';
 
 class ShowRoverImages extends StatelessWidget {
@@ -21,27 +22,28 @@ class ShowRoverImages extends StatelessWidget {
         builder: (context, state) {
       if (state is LoadingState) {
         return _onLoading();
-      } else
-        return _showDummyImages();
+      } else if (state is FetchImageState) return _showImages(state.roverData);
+
+      return Container();
     });
   }
 
-  Widget _showDummyImages() {
+  Widget _showImages(RoverData roverData) {
     return Scaffold(
-      appBar: CustomAppBar(),
-      body: GridView.count(
-        crossAxisCount: 2,
-        children: List.generate(100, (index) {
-          return Center(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Image.asset("assets/images/opportunity.webp",
-                  height: 300, fit: BoxFit.fill),
-            ),
-          );
-        }),
-      ),
-    );
+        appBar: CustomAppBar(),
+        body: GridView.builder(
+          itemCount: roverData.photos.length,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2, crossAxisSpacing: 4.0, mainAxisSpacing: 4.0),
+          itemBuilder: (BuildContext context, int index) {
+            return ClipRRect(
+                borderRadius: BorderRadius.circular(8.0),
+                child: Image.network(
+                  roverData.photos[index].img_src,
+                  fit: BoxFit.fill,
+                ));
+          },
+        ));
   }
 
   Widget _onLoading() {
